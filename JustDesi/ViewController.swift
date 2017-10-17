d0a0e95ba1d379bba7a661952232d40b90428055
Vamsi Kallepalli <vkallepalli@uworld.com>
2017-10-16 21:34:14 -0500

    Initial Commit

---
+++ JustDesi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JustDesi
+//
+//  Created by Vamsi Kallepalli on 10/16/17.
+//  Copyright © 2017 Vamsi Kallepalli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
